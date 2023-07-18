f92eebc0c2a1f011acccc7f4d1011069b54f34d7
YifanCui <18810669189@163.com>
2023-07-18 12:35:39 +0800

    [ADD] day2:拦截器实现方法
    - 创建拦截器LoginCheckFilter, 实现java.servlet.Filter的doFilter方法, 对request和response拦截

---
+++ Java/ReggieTakeOut/MavenProject/reggie_take_out/src/main/java/com/itheima/reggie/filter/LoginCheckFilter.java
@@ -0,0 +1,24 @@
+package com.itheima.reggie.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebFilter(filterName = "loginCheckFilter",urlPatterns = "/*")
+@Slf4j
+@Component
+public class LoginCheckFilter implements Filter  {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+        log.info("拦截到请求:{}",httpServletRequest.getRequestURI());
+        filterChain.doFilter(httpServletRequest,httpServletResponse);
+    }
+}
