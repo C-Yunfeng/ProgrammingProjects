09c6b993a6e5b13ae485a68d21533e60d07d77ec
YifanCui <18810669189@163.com>
2023-07-24 23:36:42 +0800

    [ADD] 用户禁用与启用
    1. v-if: 根据username判断是否展示“启动/禁用“
    2. ajax: statusHandle-enableOrDisableEmployee
    3. JaksonObjectMapper: js最大支持16位,超过会丢失精度. 因此需要将Long统一转为String

--- Java/ReggieTakeOut/MavenProject/reggie_take_out/src/main/java/com/itheima/reggie/config/WebMvcConfig.java
+++ Java/ReggieTakeOut/MavenProject/reggie_take_out/src/main/java/com/itheima/reggie/config/WebMvcConfig.java
@@ -1,9 +1,14 @@
 package com.itheima.reggie.config;
 
+import com.itheima.reggie.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -16,5 +21,19 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         // 页面访问路径/backend/**，则映射到./resources/backend/
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+
+    /**
+     * 扩展mvc框架的消息转换器
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        log.info("扩展消息转换器...");
+        // 创建消息转换器对象：把R对象转换为JSON
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+        // 设置对象转换器，地秤使用Jakson将对象转为json
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+        // 将消息转换器追加到mvc框架中，index放到0优先使用自己定义的，而非框架的
+        converters.add(0,messageConverter);
     }
 }
