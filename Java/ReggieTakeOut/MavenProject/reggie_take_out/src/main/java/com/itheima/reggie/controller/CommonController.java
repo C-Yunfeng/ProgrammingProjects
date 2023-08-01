badb5c42ad29cae8de7b21647a48813d3c3ae202
YifanCui <18810669189@163.com>
2023-08-01 23:18:42 +0800

    文件下载 CommonController.download(String name,HttpServletResponse response) FileInputStream response.getOutputStream()

--- Java/ReggieTakeOut/MavenProject/reggie_take_out/src/main/java/com/itheima/reggie/controller/CommonController.java
+++ Java/ReggieTakeOut/MavenProject/reggie_take_out/src/main/java/com/itheima/reggie/controller/CommonController.java
@@ -3,12 +3,17 @@ package com.itheima.reggie.controller;
 import com.itheima.reggie.common.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -51,5 +56,32 @@ public class CommonController {
             throw new RuntimeException(e);
         }
         return R.success(newFilename);
+    }
+
+    @GetMapping("/download")
+    public void download(String name, HttpServletResponse response){
+        try {
+            // 输入流
+            FileInputStream fileInputStream = new FileInputStream(fileDir + name);
+
+            // 输出流,将文件写回浏览器
+            ServletOutputStream outputStream = response.getOutputStream();
+
+            response.setContentType("image/jpeg");
+
+            int len = 0;
+            byte[] bytes = new byte[1024];
+            while((len = fileInputStream.read(bytes)) != -1){
+                outputStream.write(bytes,0,len);
+                outputStream.flush();
+            }
+
+            // 关闭资源
+            outputStream.close();
+            fileInputStream.close();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
