e8d42be89968aecad6ae048fd0036206d32c746f
YifanCui <18810669189@163.com>
2023-08-08 21:40:47 +0800

    新增套餐 SetmealController.save SetmealService.saveWithDish

--- Java/ReggieTakeOut/MavenProject/reggie_take_out/src/main/java/com/itheima/reggie/controller/SetmealController.java
+++ Java/ReggieTakeOut/MavenProject/reggie_take_out/src/main/java/com/itheima/reggie/controller/SetmealController.java
@@ -7,6 +7,7 @@ import com.itheima.reggie.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,8 +30,10 @@ public class SetmealController {
      * @return
      */
     @PostMapping
-    public R<String> save(SetmealDto setmealDto){
+    public R<String> save(@RequestBody SetmealDto setmealDto){
         log.info("新增套餐:{}",setmealDto);
+
+        setmealService.saveWithDish(setmealDto);
 
         return null;
     }
--- Java/ReggieTakeOut/MavenProject/reggie_take_out/src/main/java/com/itheima/reggie/service/SetmealService.java
+++ Java/ReggieTakeOut/MavenProject/reggie_take_out/src/main/java/com/itheima/reggie/service/SetmealService.java
@@ -1,7 +1,13 @@
 package com.itheima.reggie.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.itheima.reggie.dto.SetmealDto;
 import com.itheima.reggie.entity.Setmeal;
 
 public interface SetmealService extends IService<Setmeal> {
+    /**
+     * 新增套餐，并保存套餐与菜品关系
+     * @param setmealDto
+     */
+    public void saveWithDish(SetmealDto setmealDto);
 }
