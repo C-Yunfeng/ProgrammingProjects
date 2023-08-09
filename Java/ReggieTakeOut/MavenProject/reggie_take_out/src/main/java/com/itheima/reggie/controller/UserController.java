60e58c9fd02bcb1eb02a313832a02f3ceb69d879
YifanCui <18810669189@163.com>
2023-08-09 22:54:36 +0800

    短信验证 UserController.login：session保存

--- Java/ReggieTakeOut/MavenProject/reggie_take_out/src/main/java/com/itheima/reggie/controller/UserController.java
+++ Java/ReggieTakeOut/MavenProject/reggie_take_out/src/main/java/com/itheima/reggie/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.itheima.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.entity.User;
 import com.itheima.reggie.service.UserService;
@@ -16,6 +17,7 @@ import com.itheima.reggie.utils.SMSUtils;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -46,6 +48,45 @@ public class UserController {
         }
 
         return R.error("验证码发送失败");
+    }
+
+    /**
+     * 移动端用户登陆
+     * @param map
+     * @param session
+     * @return
+     */
+    @PostMapping("/login")
+    public R<User> login(@RequestBody Map map, HttpSession session){
+        log.info(map.toString());
+
+        // 获取手机号
+        String phone = map.get("phone").toString();
+
+        // 获取验证码
+        String code = map.get("code").toString();
+
+        // 从Session中获取保存的验证码
+        Object codeInSession = session.getAttribute(phone);
+
+        // 若验证码比对成功,则登陆成功
+        if(codeInSession !=null && codeInSession.equals(code)){
+            // 若为新用户,则自动注册
+            LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+            queryWrapper.eq(User::getPhone,phone);
+            User user = userService.getOne(queryWrapper);
+            if (user==null){
+                user = new User();
+                user.setPhone(phone);
+                user.setStatus(1);
+                userService.save(user);
+            }
+            // 如果不加，登陆后退回登陆页面，因为被拦截了
+            session.setAttribute("user",user.getId());
+            return R.success(user);
+        }
+
+        return R.error("登陆失败");
     }
 
 }
