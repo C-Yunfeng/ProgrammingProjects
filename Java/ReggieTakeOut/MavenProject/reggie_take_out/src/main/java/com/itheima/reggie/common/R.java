2bbce34308a1ae8b0c13565e70501ac2cb23a7a8
YifanCui <18810669189@163.com>
2023-09-04 22:52:13 +0800

    SpringCache缓存套餐数据

--- Java/ReggieTakeOut/MavenProject/reggie_take_out/src/main/java/com/itheima/reggie/common/R.java
+++ Java/ReggieTakeOut/MavenProject/reggie_take_out/src/main/java/com/itheima/reggie/common/R.java
@@ -1,12 +1,14 @@
 package com.itheima.reggie.common;
 
 import lombok.Data;
+
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
 // 加@Data注解就相当于getter setter toString了
 @Data
-public class R<T> {
+public class R<T> implements Serializable {
 
     private Integer code; //编码：1成功，0和其它数字为失败
 
--- Java/ReggieTakeOut/MavenProject/reggie_take_out/src/main/java/com/itheima/reggie/controller/SetmealController.java
+++ Java/ReggieTakeOut/MavenProject/reggie_take_out/src/main/java/com/itheima/reggie/controller/SetmealController.java
@@ -13,6 +13,8 @@ import com.sun.org.apache.xerces.internal.impl.xpath.XPath;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -40,6 +42,7 @@ public class SetmealController {
      * @return
      */
     @PostMapping
+    @CacheEvict(value = "setmealCache",allEntries = true)
     public R<String> save(@RequestBody SetmealDto setmealDto){
         log.info("新增套餐:{}",setmealDto);
 
@@ -92,6 +95,7 @@ public class SetmealController {
      * 删除套餐
      */
     @DeleteMapping
+    @CacheEvict(value = "setmealCache",allEntries = true)
     public R<String> delete(@RequestParam List<Long> ids){
         log.info("删除id为:{}",ids);
 
@@ -101,6 +105,7 @@ public class SetmealController {
     }
 
     @GetMapping("/list")
+    @Cacheable(value = "setmealCache",key = "#setmeal.categoryId + '_' + #setmeal.status")
     public R<List<Setmeal>> list(Setmeal setmeal){
         LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.eq(setmeal.getCategoryId()!=null,Setmeal::getCategoryId,setmeal.getCategoryId());
